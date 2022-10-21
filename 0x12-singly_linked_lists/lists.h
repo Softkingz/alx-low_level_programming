0d0b07fc40ae0c7529d27ffdf66e0e92ae55730c
Softkingz <knalexkingsley@gmail.com>
2022-10-21 01:54:37 +0000

    Stingly linked list Header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,8 @@
+
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
-* Desc: header file containinf prototypes and definitions
-*	for all functions and types written in this proj
-*/
-
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -15,10 +11,11 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for Holberton project
+ * for Holberton project
  */
 typedef struct list_s
 {
+
 	char *str;
 	unsigned int len;
 	struct list_s *next;
@@ -29,5 +26,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /* LISTS_H */
+#endif
