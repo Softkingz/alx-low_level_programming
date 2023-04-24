091960d717f51a1e88c187b04cce4963636e4950
Softkingz <knalexkingsley@gmail.com>
2023-04-24 06:08:28 -0700

    my firt code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
 */
 int main(void)
 {
-	printf("and that piece of art is useful\n\",-Dora Korpar,\n 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 	return (1);
 }
 
