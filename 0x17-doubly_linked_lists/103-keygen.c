161434a28659faa893448e80017169fa6ff08187
Softkingz <knalexkingsley@gmail.com>
2024-03-22 04:52:55 +0000

    doubly_linked_lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,125 +3,80 @@
 #include <time.h>
 
 /**
- * f4 - finds the biggest number
+ * generate_key - Generates a key based on the given username
  *
- * @usrn: username
- * @len: length of username
- * Return: the biggest number
+ * @username: The username to generate the key for
+ * @length: The length of the username
+ * Return: The generated key
  */
-int f4(char *usrn, int len)
-{
-	int ch;
-	int vch;
-	unsigned int rand_num;
+char *generate_key(char *username, int length) {
+    static char keygen[7];
+    int i;
+    long alph[] = {
+        0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+        0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+        0x723161513346655a, 0x6b756f494b646850
+    };
 
-	ch = *usrn;
-	vch = 0;
+    // f1
+    keygen[0] = ((char *)alph)[(length ^ 59) & 63];
 
-	while (vch < len)
-	{
-		if (ch < usrn[vch])
-			ch = usrn[vch];
-		vch += 1;
-	}
+    // f2
+    int sum = 0;
+    for (i = 0; i < length; i++)
+        sum += username[i];
+    keygen[1] = ((char *)alph)[(sum ^ 79) & 63];
 
-	srand(ch ^ 14);
-	rand_num = rand();
+    // f3
+    int product = 1;
+    for (i = 0; i < length; i++)
+        product *= username[i];
+    keygen[2] = ((char *)alph)[(product ^ 85) & 63];
 
-	return (rand_num & 63);
-}
+    // f4
+    srand(*username ^ 14);
+    int rand_num = rand() & 63;
+    keygen[3] = ((char *)alph)[rand_num];
 
-/**
- * f5 - multiplies each char of username
- *
- * @usrn: username
- * @len: length of username
- * Return: multiplied char
- */
-int f5(char *usrn, int len)
-{
-	int ch;
-	int vch;
+    // f5
+    sum = 0;
+    for (i = 0; i < length; i++)
+        sum += username[i] * username[i];
+    keygen[4] = ((char *)alph)[(sum ^ 239) & 63];
 
-	ch = vch = 0;
+    // f6
+    rand_num = rand();
+    for (i = 0; i < *username; i++)
+        rand_num = rand();
+    keygen[5] = ((char *)alph)[(rand_num ^ 229) & 63];
 
-	while (vch < len)
-	{
-		ch = ch + usrn[vch] * usrn[vch];
-		vch += 1;
-	}
+    keygen[6] = '\0';
 
-	return (((unsigned int)ch ^ 239) & 63);
-}
-
-/**
- * f6 - generates a random char
- *
- * @usrn: username
- * Return: a random char
- */
-int f6(char *usrn)
-{
-	int ch;
-	int vch;
-
-	ch = vch = 0;
-
-	while (vch < *usrn)
-	{
-		ch = rand();
-		vch += 1;
-	}
-
-	return (((unsigned int)ch ^ 229) & 63);
+    return keygen;
 }
 
 /**
  * main - Entry point
  *
- * @argc: arguments count
- * @argv: arguments vector
+ * @argc: Arguments count
+ * @argv: Arguments vector
  * Return: Always 0
  */
-int main(int argc, char **argv)
-{
-	char keygen[7];
-	int len, ch, vch;
-	long alph[] = {
-		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
-		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
-		0x723161513346655a, 0x6b756f494b646850 };
-	(void) argc;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <username>\n", argv[0]);
+        return 1;
+    }
 
-	for (len = 0; argv[1][len]; len++)
-		;
-	/* ----------- f1 ----------- */
-	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
-	/* ----------- f2 ----------- */
-	ch = vch = 0;
-	while (vch < len)
-	{
-		ch = ch + argv[1][vch];
-		vch = vch + 1;
-	}
-	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
-	/* ----------- f3 ----------- */
-	ch = 1;
-	vch = 0;
-	while (vch < len)
-	{
-		ch = argv[1][vch] * ch;
-		vch = vch + 1;
-	}
-	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
-	/* ----------- f4 ----------- */
-	keygen[3] = ((char *)alph)[f4(argv[1], len)];
-	/* ----------- f5 ----------- */
-	keygen[4] = ((char *)alph)[f5(argv[1], len)];
-	/* ----------- f6 ----------- */
-	keygen[5] = ((char *)alph)[f6(argv[1])];
-	keygen[6] = '\0';
-	for (ch = 0; keygen[ch]; ch++)
-		printf("%c", keygen[ch]);
-	return (0);
+    char *username = argv[1];
+    int length = 0;
+    while (username[length])
+        length++;
+
+    char *key = generate_key(username, length);
+
+    printf("%s\n", key);
+
+    return 0;
 }
+
